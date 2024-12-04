b39e050e737905cc7bd110c4c553258684636651
NSSF <NSSF@DESKTOP-MJ9BJME>
2024-12-04 00:08:46 -0800

    Add project files.

---
+++ Parenthesis/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Parenthesis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string unclean = "a)b(c)d"; //Input one
+            string unclean2 = "))(("; //input two
+
+            string cleaned = CleanExtra(unclean2);
+        }
+        public static string CleanExtra(string intVal)
+        {
+            string valid;
+            if (intVal == null)
+            {
+                throw new System.ArgumentException("Value cannot be null", "original");
+            }
+            else
+            {
+    
+                valid = Regex.Replace(intVal, " ", "").Replace("(", "").Replace(")", "").Replace(",", "").Replace("/", "").Replace("-", "");
+            }
+
+            return valid;
+        }
+    }
+}
